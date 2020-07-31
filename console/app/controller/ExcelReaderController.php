58998adab61327a30ca386f13fe1712a379708f6
Joseph paul <josephpaul0487@gmail.com>
2020-07-31 11:51:29 +0530

    updated

--- console/app/controller/ExcelReaderController.php
+++ console/app/controller/ExcelReaderController.php
@@ -587,32 +587,8 @@ class ExcelReaderController {
         return $response;
     }
 
-    public function cleanInput( $input ) {
-    
-        $search = array(
-            '@<script[^>]*?>.*?</script>@si', // Strip out javascript
-            '@<[\/\!]*?[^<>]*?>@si', // Strip out HTML tags
-            '@<style[^>]*?>.*?</style>@siU', // Strip style tags properly
-            '@<![\s\S]*?--[ \t\n\r]*>@',// Strip multi-line comments
-            // "#[^\\\\\\\\]'#"   
-        );
-    
-        $output = preg_replace( $search, '', $input );
-        return $output;
-    }
-
     
     public function sanitize( $input) {
-        
-        if( is_array( $input ) ) {
-            foreach( $input as $var => $val ) {
-                $output[ $var ] = $this->sanitize( $val );
-            }
-        } else {
-           
-            $input  = $this->cleanInput( $input );
-            $input = $this->connection-> escapeString( $input  );
-        }
-        return $input;
+        return $this->connection->sanitize($input);
     }
 }
